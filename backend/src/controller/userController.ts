2ed96808abf540153b4f450f041f768ab47f4c3c
Yeasir Arafat <ysarafat.bd@gmail.com>
2026-01-18 23:06:44 +0600

    feat: implement chat and message retrieval, and user listing route

---
+++ backend/src/controller/userController.ts
@@ -0,0 +1,22 @@
+import type { NextFunction, Response } from "express";
+import type { AuthRequest } from "../middleware/auth";
+import { User } from "../model/User";
+
+// get users
+export async function getUsers(
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    const userId = req.userId;
+    const users = await User.find({ _id: { $ne: userId } })
+      .select("name email avatar")
+      .limit(50);
+
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500);
+    next(error);
+  }
+}
--- backend/src/routes/chatRoutes.ts
+++ backend/src/routes/chatRoutes.ts
@@ -1,5 +1,11 @@
 import { Router } from "express";
+import { getChats, getOrCreateChat } from "../controller/chatController";
+import { protectRoute } from "../middleware/auth";
 
 const router = Router();
+router.use(protectRoute);
+
+router.get("/", getChats);
+router.post("/with/:participantId", getOrCreateChat);
 
 export default router;
